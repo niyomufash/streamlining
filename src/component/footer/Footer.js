26574586d1f8f7994925cf1d6c871fd880d93831
Niyomufasha <fabrice1niyo@gmail.com>
2023-04-06 06:20:36 -0700

    created footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -57,7 +57,7 @@ function Footer() {
 
         </div>
        
-        <div className='grid md:grid-cols-1 gap-3 pt-[10rem]'> 
+        <div className='grid md:grid-cols-1 gap-3 pt-[3rem]'> 
         <div className='flex gap-2 text-white md:justify-start '>
             <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.5" y2="6.5"></line></svg>
           <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
